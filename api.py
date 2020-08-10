f9616363f60af53c12a28bc581b25bd500b41b4e
Lucas Vasconcelos <lucaslou4@protonmail.com>
2020-08-10 10:58:33 -0300

    source environment variables from secret manager

--- api.py
+++ api.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from os import environ
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+load_dotenv()
 
 
 @app.route('/')
 def index():
     return 'hello'
+
+
+@app.route('/secret')
+def secret():
+    return environ.get('TESTE_SECRET')
 
 
 app.run('0.0.0.0', environ.get('PORT') or 8000)
